34adca5d444a9730c9cde93856bde941815fe7f1
nhpf <nicholas@hopf.pt>
2023-04-16 10:28:58 +0100

    Finished styling of login and signup enterprise pages

--- project/enterprise/app.vue
+++ project/enterprise/app.vue
@@ -1,19 +1,31 @@
 <template>
   <div class="bg-container">
-    <div class="top-gradient"/>
-    <div style="width: 100%; position: relative; z-index: 2;">
-      <RouterView/>
+    <div class="top-gradient" />
+    <img
+      src="/favicon.ico"
+      width="38"
+      alt="favicon"
+      style="position: absolute; top: 12px; left: 10px"
+    />
+    <h1 style="position: absolute; top: 10px; left: 50px">EnterpriseVault</h1>
+    <div
+      style="width: 100%; height: 100%; position: relative; z-index: 2"
+      class="is-flex is-align-items-center is-justify-content-center"
+    >
+      <RouterView />
     </div>
-    <div class="bottom-gradient"/>
+    <div class="bottom-gradient" />
   </div>
 </template>
 
-<script setup lang="ts">
-</script>
+<script setup lang="ts"></script>
 
 <style lang="sass">
 @import "@/assets/styles.sass"
 @import "bulma/bulma.sass"
+h1
+  font-size: xx-large
+  font-weight: bold
 .bg-container
   background-color: $background
   height: 100vh
--- project/enterprise/pages/login.vue
+++ project/enterprise/pages/login.vue
@@ -1,50 +1,70 @@
 <template>
-  <div class="section is-flex is-flex-direction-column is-fullwidth is-fullheight is-align-items-center">
+  <div
+    class="section is-flex is-flex-direction-column is-fullwidth is-fullheight is-align-items-center"
+  >
     <div class="card is-centered">
       <div class="card-content">
-        <p class="title">
-          Log into your vault
-        </p>
+        <p class="title">Log into your vault</p>
         <div>
           <div class="field">
-            <label class="label">Name</label>
-            <div class="control">
-              <input class="input" type="text" placeholder="Text input">
-            </div>
-          </div>
-
-          <div class="field">
             <label class="label">Email</label>
-            <div class="control has-icons-left has-icons-right">
-              <input class="input is-danger" type="email" placeholder="Email input" value="hello@">
-              <span class="icon is-small is-left">
-                <i class="fas fa-envelope"></i>
-              </span>
-              <span class="icon is-small is-right">
-                <i class="fas fa-exclamation-triangle"></i>
-              </span>
+            <div class="control">
+              <input
+                class="input"
+                :class="{
+                  'is-danger': email.length && !isEmailValid,
+                  'is-success': email.length && isEmailValid,
+                }"
+                type="email"
+                placeholder="Email input"
+                v-model="email"
+              />
             </div>
-            <p class="help is-danger">This email is invalid</p>
+            <p v-if="email.length && !isEmailValid" class="help is-danger">
+              This email is invalid
+            </p>
           </div>
 
           <div class="field">
             <label class="label">Password</label>
-            <div class="control has-icons-left has-icons-right">
-              <input class="input is-success" type="text" placeholder="Password" value="bulma">
-              <span class="icon is-small is-left">
-                <i class="fas fa-user"></i>
-              </span>
-              <span class="icon is-small is-right">
-                <i class="fas fa-check"></i>
+            <div class="control has-icons-right">
+              <input
+                class="input"
+                :class="{
+                  'is-danger': isPasswordTooShort || isPasswordTooLong,
+                  'is-success':
+                    password.length &&
+                    !isPasswordTooShort &&
+                    !isPasswordTooLong,
+                }"
+                :type="displayPassword ? 'text' : 'password'"
+                placeholder="Password"
+                v-model="password"
+              />
+              <span
+                v-if="password.length"
+                class="icon is-small is-right is-action"
+                @click="displayPassword = !displayPassword"
+              >
+                <img src="@/assets/icons/eye.svg" alt="toogle visibility" />
               </span>
             </div>
-            <p class="help is-success">This username is available</p>
+            <p v-show="isPasswordTooShort" class="help is-danger">
+              Your password is too short!
+            </p>
+            <p v-show="isPasswordTooLong" class="help is-danger">
+              Your password is too long!
+            </p>
           </div>
         </div>
       </div>
       <footer class="card-footer">
         <div class="card-footer-item">
-          <button class="button is-primary is-medium" style="width: 200px">
+          <button
+            class="button is-primary is-medium"
+            style="width: 200px"
+            :disabled="!isEmailValid || isPasswordTooShort || isPasswordTooLong"
+          >
             Login
           </button>
         </div>
@@ -59,5 +79,33 @@
   </div>
 </template>
 
-<script setup lang="ts">
+<script lang="ts">
+import { ref, computed } from "vue";
+export default {
+  setup() {
+    const email = ref("");
+    const password = ref("");
+    const displayPassword = ref(false);
+
+    const isEmailValid = computed(() => {
+      const emailRegex =
+        /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+      return emailRegex.test(email.value);
+    });
+
+    const isPasswordTooShort = computed(
+      () => password.value.length > 0 && password.value.length < 6
+    );
+    const isPasswordTooLong = computed(() => password.value.length > 20);
+
+    return {
+      email,
+      password,
+      displayPassword,
+      isEmailValid,
+      isPasswordTooShort,
+      isPasswordTooLong,
+    };
+  },
+};
 </script>
